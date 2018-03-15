749dea430839df3623b83eda4c9bebe0da41af73
vignesh <vignesh.r@lxisoft.com>
2018-03-15 09:23:32 +0530

    NW-12

--- game/SnakeNLadder.java
+++ game/SnakeNLadder.java
@@ -2,20 +2,15 @@ package com.lxisoft.snakeandladder.game;
 public class SnakeNLadder
 {
 private int noOfBox=100;
-
-
-//Snake snake=new Snake();
+Snake snake=new Snake();
 
 //Ladder ladder=new Ladder();
 
-
 public void startGame()
 {
-		
-	
-	for( int i=9;i>=0;i--)
+		for( int i=9;i>=0;i--)
 		//for( int i=100;i>0;i--)
-	{
+		{
 		if(i%2==1)
 		{
 			//for(int j=0;j<=9;j++)		
@@ -25,8 +20,6 @@ public void startGame()
 			 System.out.print("_");		
 		}
 		System.out.println("");		
-	
-		
 		System.out.print("  |");		
 		for(int x=9*9;x>2;x--)
 		{
@@ -46,6 +39,7 @@ public void startGame()
 			for(int j=9;j>=0;j--)
 			{
 				int k=(10*i)+j+1;	
+				
 				System.out.print("["+get2DigitNum(k)+"]"+"   |");
 			}
 			System.out.println("");
@@ -112,21 +106,58 @@ public String get2DigitNum(int k)
 	}
 }
 
-/*public void inputPlayersDetails()
+public void inputPlayersDetails()
 {
 	int player1=0,player2=0;
 	int currentplayer=-1;
 	int diceValue=0;
 	Scanner scan=new Scanner(System.in);
 	String str;
+	int diceValue=0;
 	
 	do
 	{
 		System.out.println(Current Player==-1? "\n FIRST PLAYER TURN" : "\n SECOND PLAYER TURN");
 		System.out.println("PRESS A TO ROLL THE DIE");
 		str=next();
+		diceValue=rollDice();
 		
-	}*/
+		if(current player==-1)
+		{
+			player1=PlayerValue(player1,diceValue);
+			System.out.println("First Player:"+player1);
+			System.out.println("Second Player"+player2);
+			System.out.println("**********************");
+		}
+		if(isWin(player1))
+		{
+			System.out.println("First Player wins");
+			return;
+		}
+		
+	}
+	else{
+		player2=playerValue(player2,diceValue);
+		System.out.println("First Player:"+player);
+		System.out.println("Second Player:"+player2);
+		System.out.println("***********************");
+		}
+		if(isWin(player2))
+		{
+			System.out.println("Second Player wins");
+			return;
+		}
+		currentPlayer=currentPlayer;
+		}while("A".equals(str));
+}	
+	
+	public int rollDice()
+	{
+		int n=0;
+		Random r=new Random();
+		n=r.nextInt(6);
+		return (n==0);
+	}
 }
 
 
---
+++ ladder/Ladder.java
@@ -0,0 +1,28 @@
+public class Ladder
+{
+private int climbUp
+private int climbDown
+
+
+Scanner scan=new Scanner(System.in);
+
+	public void setClimbUp(int climbup)
+	{
+		this.climbUp=climbUp;
+	}
+	public int getClimbUp()
+	{
+		return climbUp;
+	}
+	
+
+	public void setClimbDown(int climbDown)																				
+	{
+		this.climbDown=climbDown;
+	}
+	public int getClimbDown()
+	{
+		return climbDown;
+	}
+
+}
